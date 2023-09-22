3e3230e5ab3d50b7b0958873801feeacd35dff00
GiuliaMarcelaS <giulia.faria@itec.ufpa.br>
2023-09-22 12:03:03 -0300

    implemementando chart

--- lib/screens/graphic_screen.dart
+++ lib/screens/graphic_screen.dart
@@ -1,29 +1,99 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
 
 import '../models/graphic.dart';
 
 class GraphicScreen extends StatelessWidget {
-  GraphicScreen(//this.recentAlphabetClicks, 
+  List sums = [1,2,3,4,5,6,7,8,9,10,11,12];
+  GraphicScreen(
   {super.key});
 
- // final List recentAlphabetClicks;
-  final List tabs = ['alphabet','words','numbers', 'expressions'];
-  var aba;
-  var clicks;
-  List<Map<String, Object>> get groupedClicks {
-    return List.generate(
-      4,
-      (index) {
-        for(var tab in tabs){
-          aba = tab;
-        }
-
-        getClicks (int providerClicks){
-          clicks = providerClicks;
-        }
-        return {'tab': aba, 'clicks': clicks};
+  monthNumberConvertor(int month){
+    if(month == 1||month == -11){
+      return 'January';
+    }
+    else if(month == 2||month == -10){
+      return 'February';
+    }
+    else if(month == 3||month == -9){
+      return 'March';
+    }
+    else if(month == 4||month == -8){
+      return 'April';
+    }
+    else if(month == 5||month == -7){
+      return 'May';
+    }
+    else if(month == 6||month == -6){
+      return 'June';
+    }
+    else if(month == 7||month == -5){
+      return 'July';
+    }
+    else if(month == 8||month == -4){
+      return 'August';
+    }
+    else if(month == 9||month == -3){
+      return 'September';
+    }
+    else if(month == 10||month == -2){
+      return 'October';
+    }
+    else if(month == 11||month == -1){
+      return 'November';
+    }
+    else if(month == 12||month == 0){
+      return 'December';
+    }
+  }
+  monthStringConvertor(String month){
+    if(month == 'January'){
+      return 1;
+    }
+    else if(month == 'February'){
+      return 2;
+    }
+    else if(month == 'March'){
+      return 3;
+    }
+    else if(month == 'April'){
+      return 4;
+    }
+    else if(month == 'May'){
+      return 5;
+    }
+    else if(month == 'June'){
+      return 6;
+    }
+    else if(month == 'July'){
+      return 7;
+    }
+    else if(month == 'August'){
+      return 8;
+    }
+    else if(month == 'September'){
+      return 9;
+    }
+    else if(month == 'October'){
+      return 10;
+    }
+    else if(month == 'November'){
+      return 11;
+    }
+    else if(month == 'December'){
+      return 12;
+    }
+  }
+  List<Map<String, Object>> get groupedAlphabetClicks {
+    return List.generate(6, (index) {
+      final month = monthNumberConvertor((DateTime.now().month)-index);
+      final alphabetClicks = sums[index];
+     // print(month);
+     // print(alphabetClicks);
+        return {'month': month, 'AlphabetClicks': alphabetClicks};
       },
     );
   }
@@ -35,7 +105,9 @@ class GraphicScreen extends StatelessWidget {
         elevation: 6,
         margin: EdgeInsets.all(20),
         child: Row(
-          children: [],
+          children: groupedAlphabetClicks.map((clicks){
+            return Text('${clicks['month']}:${clicks['AlphabetClicks']}');
+          }).toList(),
         ),
       ),
     );
